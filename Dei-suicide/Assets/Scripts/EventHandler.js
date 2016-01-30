cc9598b8593a5ab33c49e75b9ed554e3241b77cb
InvictusAmicus <theinvictuscomplex@gmail.com>
2016-01-30 02:36:19 +0000

    Tree in place, labels on screen
    Second Label needs to be moved underneath

---
+++ Dei-suicide/Assets/Scripts/EventHandler.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var devotion: int;
+var development: int;
+var whatever : GUIStyle;
+var position1 : Rect = new Rect(0, 0, 200, 100);
+var position2 : Rect = new Rect(0, 100, 200, 100);
+ 
+
+function Start ()
+{
+	devotion = 30;
+	development = 0;
+
+	//labelForDevotion = new GUI.Label(position2, "Development: " + development);
+}
+
+function Update ()
+{
+
+}
+
+function OnGUI()
+{
+	GUI.Box(position1, "Devotion: " + devotion, whatever);
+	GUI.Box(position2, "Development: " + development, whatever);
+
+//	var labelForDevotion : GUI;
+//	var labelForDevelopment: GUI;
+//	labelForDevotion.Label(position1, "Devotion: " + devotion);
+}
---
+++ Dei-suicide/Assets/Scripts/TreeScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var GUIScript : EventHandler;
+var playObj : GameObject;
+var curScript : cursorScript;
+
+
+function Start ()
+{
+	playObj = GameObject.Find("GUIController");
+	GUIScript = playObj.GetComponent(EventHandler);
+}
+
+function Update ()
+{
+}
+
+function onMouseDown()
+{
+	GUIScript.devotion+= 10;
+}

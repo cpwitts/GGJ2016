b37eb2599d370a6e5bf59aaf3bb29ffef20d624e
cpwitts <cpwitts@hotmail.com>
2016-01-30 00:07:19 +0000

    Group Movement
    They listen now

---
+++ Dei-suicide/Assets/Scripts/cursorScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var depth = 10.0;
+var pos : Vector3;
+ 
+function Start ()
+{
+     Cursor.visible = false;
+}
+ 
+function Update ()
+{
+     var mousePos = Input.mousePosition;
+     var wantedPos = Camera.main.ScreenToWorldPoint (Vector3 (mousePos.x, mousePos.y, depth));
+     transform.position = wantedPos;
+
+     if(Input.GetMouseButtonDown(0))
+     {
+     	pos = wantedPos;
+     }
+}
---
+++ Dei-suicide/Assets/Scripts/humanScript.js
@@ -0,0 +1,46 @@
+﻿#pragma strict
+var playObj : GameObject;
+var curScript : cursorScript;
+var speed : float = 3.0f;
+var move : boolean = false;
+var acceptanceRange : float = 1.0f;
+//This is NOT a bug workaround, I swear. Please stop judging me
+var willOfGod : int = 0;
+
+function Start () 
+{
+	playObj = GameObject.Find("Cursor");
+	curScript = playObj.GetComponent(cursorScript);
+}
+
+function Update () 
+{
+		if(Input.GetMouseButtonDown(0))
+		{
+			move = true;
+			willOfGod = 5;
+		}
+
+
+		if (move)
+		{
+			var distanceVec : Vector3 = new Vector3 ( curScript.pos.x - transform.position.x ,  curScript.pos.y - transform.position.y, 0);
+
+			if (distanceVec.magnitude < acceptanceRange && willOfGod == 0)
+			{
+				move = false;
+				print(distanceVec);
+			}
+
+			Vector3.Normalize(distanceVec);
+			transform.Translate(distanceVec * speed * Time.deltaTime);
+
+		}
+
+		if(willOfGod > 0)
+		{
+			willOfGod --;
+		}
+
+	
+}
